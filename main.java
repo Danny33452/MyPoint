c48f29edb7d247161fbd399cda53c03a8e385234
Daniel <thecow474@gmail.com>
2023-02-21 15:03:20 -0800

    L bozo

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main {
-    public static void main(String[] theArgs) {
+    public static void main(String[] args) {
         MyPointMain a = new MyPointMain();
         MyPointMain b = new MyPointMain(2,2);
         System.out.println(b.quadrant(a));
