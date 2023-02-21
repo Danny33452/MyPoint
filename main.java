6df8eaec567ba995e4124ad8ad7952d829eaaabd
Daniel <thecow474@gmail.com>
2023-02-21 15:09:44 -0800

    L bozo

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         MyPoint a = new MyPoint();
         MyPoint b = new MyPoint(2,2);
-        System.out.println(b.quadrant(a));
+        System.out.println(MyPoint.quadrant(a));
+        System.out.println(MyPoint.quadrant(new MyPoint(3,-1));
     }
 }
